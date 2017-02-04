304bca9643d2950fa1b52ec0be3dac6c706e2571
Arvid Axelsson <axelssonaf@gmail.com>
2017-02-04 02:54:32 +0100

    Split libresistance into helper functions

--- libresistance.c
+++ libresistance.c
@@ -6,29 +6,43 @@
  */
 #include "libresistance.h"
 
+static float parallel(int count, const float *array)
+{
+	double sum_inverses = 0.0;
+	for (int i = 0; i < count; ++i) {
+		if (array[i] == 0.0) return 0.0f;
+		if (array[i] < 0.0) return -1.0f;
+		sum_inverses += 1.0 / array[i];
+	}
+	return 1.0 / sum_inverses;
+}
+
+static float series(int count, const float *array)
+{
+	double sum = 0.0;
+	for (int i = 0; i < count; ++i) {
+		if (array[i] < 0.0) return -1.0f;
+		sum += array[i];
+	}
+	return sum;
+} 
+
 float calc_resistance(int count, char conn, const float *array)
 {
 	if (!array) {
 		return -1.0f;
 	}
+	float equivalent = 0.0f;
+
 	// Parallel resistances
 	if (conn == 'p' || conn == 'P') {
-		double sum_inverses = 0.0;
-		for (int i = 0; i < count; ++i) {
-			if (array[i] == 0.0) return 0.0f;
-			if (array[i] < 0.0) return -1.0f;
-			sum_inverses += 1.0 / array[i];
-		}
-		return 1.0 / sum_inverses;
+		equivalent = parallel(count, array);
 	// Resistances in series
 	} else if (conn == 's' || conn == 'S') {
-		double sum = 0.0;
-		for (int i = 0; i < count; ++i) {
-			if (array[i] < 0.0) return -1.0f;
-			sum += array[i];
-		}
-		return sum;
+		equivalent = series(count, array);
+	} else {
+		return -1.0f;
 	}
-	return -1.0f;
+	return equivalent;
 }
 
